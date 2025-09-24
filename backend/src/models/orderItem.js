f82f4b1bfa70f88fa0598d18b6cd254c69fb712b
fartash2792 <ranatashi27@gmail.com>
2025-09-24 14:01:24 +0500

    Delete backend directory

--- backend/src/models/orderItem.js
+++
@@ -1,8 +0,0 @@
-import { DataTypes } from 'sequelize'
-export function defineOrderItem(sequelize){
-  return sequelize.define('OrderItem', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    quantity: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 1 },
-    price: { type: DataTypes.INTEGER, allowNull: false }
-  })
-}
